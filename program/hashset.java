4cbdc0c82587e1fa4a4a720b46494ca2a017729d
puneethkumarhp <puneethkumarhp2001@gmail.com>
2023-07-12 19:17:35 +0530

    first hash set program

---
+++ program/hashset.java
@@ -0,0 +1,20 @@
+package program;
+import java.util.HashSet;
+public class hashset {
+
+	
+	    public static void main(String[] args) {
+	        HashSet<String> set = new HashSet<>();
+
+	        set.add("Apple");
+	        set.add("Banana");
+	        set.add("Orange");
+	        set.add("Grapes");
+	        set.add("Mango");
+
+	        int size = set.size();
+	        System.out.println("Number of elements in the HashSet: " + size);
+	    }
+	}
+
+
